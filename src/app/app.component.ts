3bb9616868d7c8937b044f123489f3da9e89e891
Michael Donovan <mike@eggborne.com>
2018-10-16 14:28:05 -0700

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Task } from './models/task.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title: string = 'To-Do List';
+  currentFocus: string = "Angular";
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  firstTask: Task = new Task("Suck an old homeless man's balls")
+  secondTask: Task = new Task("Dig the crack rocks out of my ass")
+}
